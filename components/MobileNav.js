375cf9a52ec90c62faab03e7f1a279da97e24266
Aaron Toliver <flux.charlotte@gmail.com>
2021-10-22 14:16:01 -0400

    mobile nav

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import { MenuIcon } from '@heroicons/react/solid';
+import MobileMenu from './MobileMenu';
 
 const MobileNav = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menu, setMenu] = useState(false)
 
-    console.log(menuOpen);
+    console.log(menu)
+
+    const toggleMenu = () => {
+        menu ? setMenu(false) : setMenu(true)
+    }
 
     return (
-        <nav className="visible md:invisible w-full px-8 py-3 fixed flex flex-row items-center justify-between bg-gray-500">
-            {/* logo */}
-            <div className="text-2xl" >
-                <a href="#">LOGO</a>
-            </div>
-            <div className="text-2xl" >
-                <MenuIcon className="h-10 w-10 cursor-pointer" onClick={() => setMenuOpen(true)}/>
-            </div>
-        </nav>
+        <div className="visible md:invisible md:z-0 fixed w-full">
+            <nav className="z-50 px-8 py-3 flex flex-row items-center justify-between bg-gray-500">
+                {/* logo */}
+                <div className="text-2xl" >
+                    <a href="#">LOGO</a>
+                </div>
+                <div className="text-2xl" >
+                    <MenuIcon className="h-10 w-10 cursor-pointer" onClick={() => toggleMenu()}/>
+                </div>
+            </nav>
+            <div className={menu ? "-translate-x-full transition duration-500 ease-in-out" : "-translate-x-0 transition duration-500 ease-in-out"}>
+                <MobileMenu className="" />
+            </div>        
+        </div>
     )
 }
 
