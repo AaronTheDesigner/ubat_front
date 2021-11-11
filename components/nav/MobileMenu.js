369290c152050b723be93f235ea3c6aa97bf36d3
Aaron Toliver <flux.charlotte@gmail.com>
2021-11-11 17:26:26 -0500

    revert nav

--- components/nav/MobileMenu.js
+++ components/nav/MobileMenu.js
@@ -6,13 +6,13 @@ const MobileMenu = ({onClick}) => {
     // use a hook => onclick for each link slides the menu back to the side.
     // if ^^^ does not work, consider passing up props
     return (
-        <div className="flex z-50 flex-col bg-gray-500 h-screen items-center justify-center overflow-y-scroll gap-y-2" role="mobile menu" >
+        <div className="flex z-50 flex-col bg-gray-500 h-screen items-center justify-center overflow-y-scroll gap-y-8" role="mobile menu" >
             {/* logo */}
-            <div className="text-xl" onClick={onClick}>
+            <div className="text-3xl" onClick={onClick}>
                 <Link href="/#">LOGO</Link>
             </div>
             {/* menu */}
-            <div className="list-none flex h-3/5 flex-col justify-center items-center gap-y-5 text-base pb-3">
+            <div className="list-none flex flex-col justify-center items-center gap-y-5 pb-3">
                 <div className="inline-block relative">
                     <li className="relative" onClick={onClick}>
                         <Link href="/#services" className="inline-block h-full w-full">Services</Link>
@@ -40,7 +40,7 @@ const MobileMenu = ({onClick}) => {
                 </div>
             </div>
             {/* social items */}
-            <div className="flex h-1/5 gap-x-4 opacity-75 pb-10">
+            <div className="flex gap-x-4 opacity-75 pb-10">
                 <Link href="#" class="p-2">
                     <svg className="w-6 h-6" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <title>Email</title>
