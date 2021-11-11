12c64bcbfe7453301a47438135f0c7a893896ec8
Aaron Toliver <flux.charlotte@gmail.com>
2021-11-11 17:41:59 -0500

    social to mobile nav from mobile menu

--- components/nav/MobileMenu.js
+++ components/nav/MobileMenu.js
@@ -6,13 +6,13 @@ const MobileMenu = ({onClick}) => {
     // use a hook => onclick for each link slides the menu back to the side.
     // if ^^^ does not work, consider passing up props
     return (
-        <div className="flex z-50 flex-col bg-gray-500 h-screen items-center justify-center overflow-y-scroll gap-y-8" role="mobile menu" >
+        <div className="flex z-50 flex-col bg-gray-500 h-screen items-center justify-center overflow-y-scroll gap-y-4" role="mobile menu" >
             {/* logo */}
-            <div className="text-3xl" onClick={onClick}>
+            <div className="text-4xl font-bold" onClick={onClick}>
                 <Link href="/#">LOGO</Link>
             </div>
             {/* menu */}
-            <div className="list-none flex flex-col justify-center items-center gap-y-5 pb-3">
+            <div className="list-none flex flex-col justify-center items-center text-2xl gap-y-5 my-7">
                 <div className="inline-block relative">
                     <li className="relative" onClick={onClick}>
                         <Link href="/#services" className="inline-block h-full w-full">Services</Link>
@@ -40,7 +40,7 @@ const MobileMenu = ({onClick}) => {
                 </div>
             </div>
             {/* social items */}
-            <div className="flex gap-x-4 opacity-75 pb-10">
+            {/* <div className="flex h-11 gap-x-4 opacity-75">
                 <Link href="#" class="p-2">
                     <svg className="w-6 h-6" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <title>Email</title>
@@ -59,7 +59,7 @@ const MobileMenu = ({onClick}) => {
                         <path d="M12 2.04001C6.5 2.04001 2 6.53001 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85001C10.44 7.34001 11.93 5.96001 14.22 5.96001C15.31 5.96001 16.45 6.15001 16.45 6.15001V8.62001H15.19C13.95 8.62001 13.56 9.39001 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5879 18.0622 20.3855 19.6099 18.5701C21.1576 16.7546 22.0054 14.4457 22 12.06C22 6.53001 17.5 2.04001 12 2.04001Z" fill="black"></path>
                     </svg>
                 </Link>
-            </div>
+            </div> */}
         </div>
     )
 }
