a32aa8f6d3781db39dc028a6737f567a93033428
Aaron Toliver <flux.charlotte@gmail.com>
2021-11-11 17:22:03 -0500

    nav bug fixed

--- components/nav/MobileNav.js
+++ components/nav/MobileNav.js
@@ -11,14 +11,14 @@ const MobileNav = () => {
     }
 
     return (
-        <div className="visible h-16 z-50 md:invisible md:z-0 fixed w-full">
+        <div className="visible z-50 md:invisible md:z-0 fixed w-full">
             <nav className="filter drop-shadow-md px-8 py-3 flex flex-row items-center justify-between bg-gray-500">
                 {/* logo */}
-                <div className="text-2xl" >
+                <div className="text-base font-extrabold" >
                     <Link href="#">LOGO</Link>
                 </div>
                 <div className="text-2xl" >
-                    <MenuIcon className="h-10 w-10 cursor-pointer" onClick={() => toggleMenu()}/>
+                    <MenuIcon className=" h-5 w-5 cursor-pointer" onClick={() => toggleMenu()}/>
                 </div>
             </nav>
             <div className={menu ? "-translate-x-0 transition duration-500 ease-in-out" : "-translate-x-full transition duration-500 ease-in-out opacity-0"}>
