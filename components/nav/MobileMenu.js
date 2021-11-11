0fc18e4e877e99cad2fc1715a48b8c7e57b62d75
Aaron Toliver <flux.charlotte@gmail.com>
2021-11-11 16:26:53 -0500

    mobile nav adjust

--- components/nav/MobileMenu.js
+++ components/nav/MobileMenu.js
@@ -6,7 +6,7 @@ const MobileMenu = (props) => {
     // use a hook => onclick for each link slides the menu back to the side.
     // if ^^^ does not work, consider passing up props
     return (
-        <div className="flex z-50 flex-col bg-gray-500 h-screen items-center justify-center gap-y-8" role="mobile menu" >
+        <div className="flex z-50 flex-col bg-gray-500 h-screen items-center justify-start gap-y-8" role="mobile menu" >
             {/* logo */}
             <div className="text-3xl" >
                 <Link href="/">LOGO</Link>
