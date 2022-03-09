572f56c2f128761e655e45ab1ee5061c3ad97fed
Aaron Toliver <flux.charlotte@gmail.com>
2022-03-08 21:47:41 -0500

    resize

--- components/LogoInline.js
+++ components/LogoInline.js
@@ -4,7 +4,7 @@ const LogoInline = () => {
   return (
     <div className="inline">
         <div className='flex items-center gap-x-2'>
-            <span className='logo font-bold text-2xl text-center equinox-bold uppercase md:text-5xl lg:text-6xl xl:text-6xl 2xl:text-7xl'>UAT</span>
+            <span className='logo font-bold text-2xl text-center equinox-bold uppercase md:text-5xl lg:text-6xl xl:text-5xl 2xl:text-7xl'>UAT</span>
             <span className='logoname tracking-wider dosis-bold uppercase md:text-base lg:text-base xl:text-base 2xl:text-2xl'>Upgrades By AT</span>
         </div>
     </div>
